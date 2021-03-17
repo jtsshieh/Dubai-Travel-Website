e332994195069312d98d1cdd3ec6e5808f2977d7
Justin <27078349+jtsshieh@users.noreply.github.com>
2021-03-17 09:58:48 -0400

    Use Google Charts for the weather chart

--- src/pages/Weather/Weather.tsx
+++ src/pages/Weather/Weather.tsx
@@ -1,6 +1,13 @@
 import ReactMarkdown from 'react-markdown';
 import renderers from '../../MaterialMDRenderer';
-import { Card, makeStyles, useTheme } from '@material-ui/core';
+import {
+	Card,
+	CircularProgress,
+	makeStyles,
+	useTheme,
+} from '@material-ui/core';
+import { Chart } from 'react-google-charts';
+import { ChartWrapperOptions } from 'react-google-charts/dist/types';
 
 const article = `The weather in Dubai is very warm. You'll find the most comfortable temperatures during the months of November to March. During those months, you can expect low temperatures of around 60 degrees Fahrenheit and highs of 80 to 90 degrees Fahrenheit. There is also little to no precipitation. 
 
@@ -15,19 +22,71 @@ Most buildings in Dubai have air conditioning so you are able to wear comfortabl
 
 const useStyles = makeStyles({
 	card: {
-		width: 600,
-		height: 371,
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		width: '100%',
+		height: 400,
 	},
 });
 
-const lightGraph =
-	'https://docs.google.com/spreadsheets/d/e/2PACX-1vQhcMfG2xuYYJ6LY9Ygf5RXdJm4-Yloqkf3MXaYTdt0sdpdVu-xUUHgO6X9sP2hqZkYFXczGkZeaKs1/pubchart?oid=2100013551&amp;format=interactive';
-const darkGraph =
-	'https://docs.google.com/spreadsheets/d/e/2PACX-1vQhcMfG2xuYYJ6LY9Ygf5RXdJm4-Yloqkf3MXaYTdt0sdpdVu-xUUHgO6X9sP2hqZkYFXczGkZeaKs1/pubchart?oid=2032549317&amp;format=interactive';
+const chartData = [
+	['Month', 'Temperature', '', '', ''],
+	['Jan', 61, 61, 76, 76],
+	['Feb', 63, 63, 79, 79],
+	['Mar', 67, 67, 84, 84],
+	['Apr', 74, 74, 92, 92],
+	['May', 80, 80, 101, 101],
+	['Jun', 85, 85, 104, 104],
+	['Jul', 89, 89, 106, 106],
+	['Aug', 89, 89, 107, 107],
+	['Sep', 84, 84, 103, 103],
+	['Oct', 78, 78, 97, 97],
+	['Nov', 71, 71, 87, 87],
+	['Dec', 64, 64, 80, 80],
+];
 
 export function Weather() {
 	const classes = useStyles();
 	const theme = useTheme();
+
+	const chartTextStyle = {
+		color: theme.palette.text.primary,
+		italic: false,
+		fontName: theme.typography.fontFamily,
+	};
+
+	const options: ChartWrapperOptions['options'] = {
+		colors: [theme.palette.primary.main],
+		chartArea: {
+			width: '85%',
+		},
+		backgroundColor: 'transparent',
+		hAxis: {
+			title: 'Month',
+			titleTextStyle: chartTextStyle,
+			gridlines: {
+				color: theme.palette.text.primary,
+			},
+			textStyle: chartTextStyle,
+		},
+		vAxis: {
+			title: 'Temperature (Fahrenheit)',
+			titleTextStyle: chartTextStyle,
+			gridlines: {
+				color: theme.palette.text.primary,
+			},
+			textStyle: chartTextStyle,
+		},
+		title: 'Weather history for Dubai, United Arab Emirates',
+
+		titleTextStyle: {
+			...chartTextStyle,
+			fontSize: 18,
+		},
+		legend: 'none',
+	};
+
 	return (
 		<div>
 			<ReactMarkdown escapeHtml={false} renderers={renderers}>
@@ -35,14 +94,13 @@ export function Weather() {
 			</ReactMarkdown>
 			<br />
 			<Card className={classes.card}>
-				<iframe
-					title="Weather history for Dubai, United Arab Emirates"
-					width="600"
-					height="371"
-					seamless
-					frameBorder="0"
-					scrolling="no"
-					src={theme.palette.type === 'light' ? lightGraph : darkGraph}
+				<Chart
+					width="100%"
+					height="100%"
+					chartType="CandlestickChart"
+					loader={<CircularProgress />}
+					data={chartData}
+					options={options}
 				/>
 			</Card>
 		</div>
